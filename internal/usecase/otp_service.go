7acbe9f7d7b1eb45e7a403448c960c017171916d
enfvs <drpsih2004@gmail.com>
2026-03-02 22:21:39 +0300

    add base struct otp service

--- internal/usecase/otp_service.go
+++ internal/usecase/otp_service.go
@@ -1,1 +1,24 @@
 package usecase
+
+import (
+	"context"
+	"github.com/WebCraftersGH/Auth-service/internal/domain"
+)
+
+type otpSVC struct{}
+
+func NewOTPSVC() *otpSVC {
+	return &otpSVC{}
+}
+
+func (s *otpSVC) GenerateOTP() domain.OTP {
+	return domain.OTP{}
+}
+
+func (s *otpSVC) SaveCode(ctx context.Context, code domain.OTP) error {
+	return nil
+}
+
+func (s *otpSVC) VerifyCode(ctx context.Context, email, code string) error {
+	return nil
+}
