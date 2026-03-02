4705e431bf446e0027ed6995eb28964244d1eb2f
enfvs <drpsih2004@gmail.com>
2026-03-02 23:08:38 +0300

    add generateOTP func

--- internal/usecase/otp_service.go
+++ internal/usecase/otp_service.go
@@ -2,17 +2,49 @@ package usecase
 
 import (
 	"context"
+	"crypto/rand"
+	"github.com/WebCraftersGH/Auth-service/internal/contracts"
 	"github.com/WebCraftersGH/Auth-service/internal/domain"
+	"math/big"
 )
 
-type otpSVC struct{}
+const (
+	LETTERS        = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	DEFAULT_LENGTH = 6
+)
 
-func NewOTPSVC() *otpSVC {
-	return &otpSVC{}
+type otpSVC struct {
+	otpRepo   contracts.OTPsRepo
+	otpLength int
+	logger    contracts.ILogger
+}
+
+func NewOTPSVC(
+	otpRepo contracts.OTPsRepo,
+	otpLength int,
+	logger contracts.ILogger,
+) *otpSVC {
+
+	if otpLength <= 0 || otpLength >= len(LETTERS) {
+		otpLength = DEFAULT_LENGTH
+	}
+
+	return &otpSVC{
+		otpRepo:   otpRepo,
+		otpLength: otpLength,
+		logger:    logger,
+	}
 }
 
 func (s *otpSVC) GenerateOTP() domain.OTP {
-	return domain.OTP{}
+
+	code := make([]byte, s.otpLength)
+	for i := 0; i < s.otpLength; i++ {
+		randInt, _ := rand.Int(rand.Reader, big.NewInt(int64(len(LETTERS))))
+		code[i] = LETTERS[randInt.Int64()]
+	}
+
+	return domain.OTP{Value: string(code)}
 }
 
 func (s *otpSVC) SaveCode(ctx context.Context, code domain.OTP) error {
